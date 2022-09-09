c2c978e1bf1925e733d4e6b71a947c05df4c89be
MallavarapuVivek <110219867+MallavarapuVivek@users.noreply.github.com>
2022-09-09 11:35:08 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+console.log('Hello aspirants');
+a=18
+b=25
+console.log("a+b is",a+b);
+function tester() {
+  var message;
+  if(confirm("Press a button!")) {
+    message = "You pressed OK!";
+  } 
+  else {
+    message = "You pressed Cancel!";
+  }
+  console.log(message);
+}
+tester();
